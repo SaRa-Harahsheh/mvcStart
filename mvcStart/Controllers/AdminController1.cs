8c817ee68f701f24bfd3d95756aef7d26a8bdd35
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-02-17 05:10:33 +0300

    Add project files.

---
+++ mvcStart/Controllers/AdminController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvcStart.Controllers
+{
+    public class AdminController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
